dd28e4529d714e98bc14315990ef0ee91ed251e1
majianting <majianting@corp.netease.com>
2020-12-31 15:54:23 +0800

    同步忙线状态枚举值

--- tool_kits/ui_component/ui_kit/util/net_call_helper.h
+++ tool_kits/ui_component/ui_kit/util/net_call_helper.h
@@ -12,9 +12,10 @@ namespace nim_comp
 	enum NIMNetCallStatus
 	{
 		kNIMNetCallStatusComplete = 1,	/**< 1:通话完成*/
-		kNIMNetCallStatusCanceled,		/**< 2:通话取消*/
-		kNIMNetCallStatusRejected,		/**< 3:通话拒绝*/
-		kNIMNetCallStatusTimeout		/**< 4:超时未接听*/
+		kNIMNetCallStatusCanceled,			/**< 2:通话取消*/
+		kNIMNetCallStatusRejected,			/**< 3:通话拒绝*/
+		kNIMNetCallStatusTimeout,			/**< 4:超时未接听*/
+		kNIMNetCallStatusBusy,					/**< 5:对方忙线*/
 	};
 
 	enum NIMNetCallType
