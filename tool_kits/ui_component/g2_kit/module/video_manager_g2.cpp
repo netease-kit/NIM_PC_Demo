27ef4f779b9d2676bcfa1cea5dfcdae02c11219b
xiaoge136 <xiaoge136@foxmail.com>
2022-01-26 17:07:44 +0800

    Update video_manager_g2.cpp
    docs: update comment

--- tool_kits/ui_component/g2_kit/module/video_manager_g2.cpp
+++ tool_kits/ui_component/g2_kit/module/video_manager_g2.cpp
@@ -111,7 +111,7 @@ namespace nim_comp
 		nbase::BatpPack bp;
 		AvChatParams params;
 		params.appKey = appkey;// nim::Client::GetCurrentUserAccount();
-		params.useRtcSafeMode = true;  //是否开启安全模式，true表示开启，false表示关闭
+		params.useRtcSafeMode = false;  //是否开启安全模式，true表示开启，false表示关闭
 		bp.head_.action_name_ = kAvChatSetupAppKey;
 		bp.body_.param_ = params;
 		nbase::BusinessManager::GetInstance()->Request(bp, nullptr);
