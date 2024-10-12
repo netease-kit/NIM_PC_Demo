5453e6d567a3a9988c5774f8caa580607b279d6d
Dylan <2894220@gmail.com>
2024-10-12 14:06:37 +0800

    feat: build Release successfully
    Signed-off-by: Dylan <2894220@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -33,7 +33,7 @@ class NebaseConan(ConanFile):
         self.requires("tinyxml/2.6.2")
         self.requires("nim/10.5.0-beta.2@yunxin/testing")
         self.requires("nertc/4.1.1@yunxin/stable")
-        self.requires("cef/2623@yunxin/stable")
+        self.requires("libcef/3.2623.1401@yunxin/stable")
         self.requires("ne_live_player/1.1.1@yunxin/stable")
         self.requires("image_ole/4.2.0@yunxin/stable")
         self.requires("ne_transfer/0.1.0@yunxin/stable")
@@ -41,7 +41,7 @@ class NebaseConan(ConanFile):
     def imports(self):
         script_path = os.path.split(os.path.realpath(__file__))[0]
         binary_path = os.path.join(script_path, "bin")
-        self.copy("*.*", binary_path, "bin", "cef")
+        self.copy("*.*", binary_path, "bin", "libcef")
         self.copy("*.*", binary_path, "bin", "nim")
         self.copy("*.*", binary_path, "bin", "nertc")
         self.copy("*.*", binary_path, "bin", "ne_live_player")
